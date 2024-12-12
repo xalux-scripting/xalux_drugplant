3493c8b4761dbcd7b1435af8c91508a693eac0ed
xalux <xaluxscripting@gmail.com>
2024-12-12 22:57:58 +0200

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,7 +1,19 @@
-CREATE TABLE `player_plants` (
-    `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `player_identifier` VARCHAR(50) NOT NULL,
-    `plant_id` INT UNIQUE NOT NULL,
-    `plant_name` VARCHAR(50) NOT NULL,
-    `interior_id` VARCHAR(50) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `plant_permissions` (
+  `plant_id` varchar(50) NOT NULL,
+  `player_identifier` varchar(255) NOT NULL,
+  `steam_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`plant_id`,`player_identifier`),
+  CONSTRAINT `plant_permissions_ibfk_1` FOREIGN KEY (`plant_id`) REFERENCES `player_plants` (`plant_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `player_plants` (
+  `plant_id` varchar(50) NOT NULL,
+  `player_identifier` varchar(50) NOT NULL DEFAULT '0',
+  `interior_id` varchar(50) NOT NULL DEFAULT '',
+  `buying_price` int(11) NOT NULL,
+  `plant_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`plant_id`),
+  KEY `idx_player_identifier` (`player_identifier`),
+  KEY `idx_interior_id` (`interior_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
