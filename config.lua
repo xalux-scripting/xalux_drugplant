8c198463ca81e15c3a038d72d500804f5404787b
xalux <xaluxscripting@gmail.com>
2024-12-12 22:56:28 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,26 +1,35 @@
 Config = {}
 
+-- frameworks
+Config.Framework = "QBX" -- Set to "ESX" or  "QBX" for the manage plants command
+
+
 Config.Interiors = {
     ["Interior1"] = {
         InsideCoords = vector3(1088.81, -3187.57, -38.99),
         CookingCoords = vector3(1090.42, -3194.9, -38.99),
+        ManagementCoords = vec3(1087.16, -3194.49, -38.99),
         drug = "cocaine", 
     },
     ["Interior2"] = {
         InsideCoords = vector3(1088.81, -3187.57, -38.99),
         CookingCoords = vector3(1090.42, -3194.9, -38.99),
+        ManagementCoords = vec3(1087.16, -3194.49, -38.99),
         drug = "cocaine", 
     },
 
+    --you can add more interiors
 }
 
-Config.debug = false
+-- processing
 
 Config.processing = {
-    CycleTime = 10000, -- how long one cycle lasts by default 10 seconds
-    TemperatureIncrease = 10, -- how much water temperature increases in one cycle
-    ExplosionThreshold = 100, -- when will the water over heat
-    EnableExplosion = false, -- Set to false to disable explosions,
+    CycleTime = 500,
+    TemperatureIncrease = 10,
+    ExplosionThreshold = 25,
+    EnableExplosion = true, -- Set to false to disable explosions,
+    EnablePoliceNotify = false,
+    ProcetaceChange = 50, -- 50% chance to dispatch police
     TemperatureResetTime = 60000, -- Time in milliseconds to reset temperature after stopping
     MinimumWaterRequired = 10, -- Minimum water (liters) required to cook
     WaterLossPerCycle = 2, -- Liters of water lost after each cycle
@@ -35,6 +44,23 @@ Config.processing = {
     }
 }
 
-Config.Framework = "QBX" -- Set to "ESX" or  "QBX" for the manage plants command
-
+--discord logs
 Config.Webhook = 'change-me'
+
+-- buying configuring
+Config.maxPlantsPerLocation = 5
+
+--selling
+Config.SellPenaltyPercent = 25
+
+--dispatch
+Config.DispatchSystem = "CodeDesign" -- Options: "CodeDesign", "Brutal", "Quasar"
+
+Config.dispatch = {
+    Code = "10-66", -- Dispatch code
+    Title = "Illegal Substance Cooking", -- Dispatch title
+    Description = "Explosion reported, possibly related to illegal activity.", -- Dispatch description
+    Sprite = 436, -- Blip sprite ID
+    Color = 1, -- Blip color
+    Scale = 1.2 -- Blip size
+}
